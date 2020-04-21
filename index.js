018dbb71b912d3b652c9fe7c135d4af26300f1a5
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-21 15:30:30 -0700

    add exercise 3.1

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3001
+
+let persons = [
+	{
+		name: "Joe",
+		number: "123123",
+		id: 1
+	},
+	{
+		name: "Joe2",
+		number: "5345",
+		id: 2
+	},
+	{
+		name: "Joe3",
+		number: "6456",
+		id: 3
+	},
+	{
+		name: "Joe4",
+		number: "8678",
+		id: 4
+	},
+]
+
+app.get('/', (req, res) => {
+	res.json({Status: 'Working!'})
+})
+
+app.get('/api', (req, res) => {
+	res.json({API: "Phonebook persons API"})
+})
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons)
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
